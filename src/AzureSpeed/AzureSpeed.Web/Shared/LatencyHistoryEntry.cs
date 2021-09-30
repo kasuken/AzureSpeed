860469a13316c924015d7f6b5c68a1f403010a44
ANTOS\alessio.marinelli <alessio.marinelli@antos.it>
2021-09-30 12:15:04 +0200

    + added ChartJS.Blazor library + LegacyTrend chart component + updated models

---
+++ src/AzureSpeed/AzureSpeed.Web/Shared/LatencyHistoryEntry.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureSpeed.Web.Shared
+{
+    public class LatencyHistoryEntry
+    {
+        public DateTime Timestamp { get; set; }
+
+        public double Latency { get; set; } 
+    }
+}
